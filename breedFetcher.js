fefbaec34e3beb4eb6699afbb75b5d31a7bb2dbf
goingdust <dustinluck@gmail.com>
2021-12-02 20:01:16 +0000

    Wrote a test file for breedFetcher with mocha and chai that tests when a valid breed is passed in and when an invalid breed is passed in

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,20 @@
 const request = require('request');
 
-const fetchBreedDescription = (query, callback) => {
+const fetchBreedDescription = (breedName, callback) => {
+  // turn breedName into a 4-character query ('Sibe' for 'Siberian')
+  const query = breedName.substring(0, 4);
   request(`https://api.thecatapi.com/v1/breeds/search?q=${query}`, (error, response, body) => {
     // turn string body content into an object with JSON
     const data = JSON.parse(body);
-    // if URL is wrong, print error message and stop function
+    // if URL does not exist, print error message and stop function
     if (error) {
       callback(error);
       return;
     }
     // if the breed is not found, print a message and stop function
     if (!data[0]) {
-      callback(null, 'Sorry, breed not found... 😵‍💫');
+      error = 'Sorry, breed not found... 😵‍💫';
+      callback(error);
       return;
     }
     // print the description of the breed
